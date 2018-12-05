36b98d704e63b21853a2175490bbef5b681b960b
Justin Terry (VM) <juterry@microsoft.com>
2018-12-05 13:52:07 -0800

    Move all end-to-end tests to the tests folder
    To better align with the golang project recommended layout moving all non-unit
    tests to the /tests folder. These require full e2e running of containers in
    various ways. All unit test's should still be written inline with the package
    under test as normal.
    
    Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

--- test/functional/uvm_mem_backingtype_test.go
+++ test/functional/uvm_mem_backingtype_test.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Microsoft/hcsshim/functional/utilities"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 	"github.com/Microsoft/hcsshim/osversion"
+	"github.com/Microsoft/hcsshim/test/functional/utilities"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sirupsen/logrus"
 )
